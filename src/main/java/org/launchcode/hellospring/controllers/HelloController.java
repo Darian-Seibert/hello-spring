86866e023cc6883b7e519a6a872727f5c2a11a05
Darian Seibert <darianwseibert@gmail.com>
2022-11-30 18:49:11 -0600

    checking to see if this works

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -2,11 +2,12 @@ package org.launchcode.hellospring.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+/*@ResponseBody
+@RequestMapping("hello")*/
 public class HelloController {
 
    /* @GetMapping("hello")
@@ -15,30 +16,26 @@ public class HelloController {
         return "Hello, Spring!";
     }*/
 
-    @GetMapping("goodbye")
+    /*@GetMapping("goodbye")
     public String goodbye() {
         return "Goodbye, Spring!";
-    }
+    }*/
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
-    @GetMapping("{name}")
+    @GetMapping("hello/{name}")
+    @ResponseBody
     public String helloWithPathParam(@PathVariable String name) {
         return "Hello, " + name + "!";
     }
 
     @GetMapping("form")
     public String helloForm() {
-        return "<html>" +
-                "<body>" +
-                "<form action='hello/' method='post'>" +
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet me!'>" +
-                "</form>" +
-                "</body>" +
-                "</html>";
+        return "form";
     }
 }
